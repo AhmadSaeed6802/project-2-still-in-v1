6a46278c748fe135ff881587018f6d2b5df47c46
AhmadSaeed6802 <139094382+AhmadSaeed6802@users.noreply.github.com>
2023-07-13 07:26:39 -0400

    Add files via upload

---
+++ 15 paramid shap/15 paramid shap/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_Right_angle_triangle_using
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {       //  know the user to enter the number of rows of stearic '*' for height of right triangle
+            Console.WriteLine("Enter the number of of rows of stearic * for height");
+            int Height = int.Parse(Console.ReadLine());
+            // iterate from 1 to Height for printing no of rows on
+            for (int i = 1; i <= Height; i++)
+            {    // Print number of spaces 
+                for (int j = 1; j <=Height- i; j++)
+                {
+                    Console.Write(" ");
+                }
+                // print stars
+                for (int k = 1; k <= 2*i-1; k++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 16 Reverse a string/16 Reverse a string/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_Reverse_a_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { // let know the user to enter a string 
+            Console.WriteLine("Enter a string:");
+            //Read the user input and store in a variable
+            String input = Console.ReadLine();
+           //call the Reverse method to reverse the string
+         string reversed = Reverse(input);
+           //display the reverse string
+           Console.WriteLine("Reversed string: " + reversed);
+            Console.ReadKey();
+        }
+        static string Reverse(string str)
+        {
+            //convert the input string  to character array
+            char[] charArray = str.ToCharArray();
+            //reverse the character array
+            Array.Reverse(charArray);
+            return new string(charArray);
+
+        }
+    }
+}
